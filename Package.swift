11ff050ed90d32e466273c132894aeb57edb33cc
Joshua Eckstein <jeckstein@autobooks.co>
2025-04-14 15:43:29 -0400

    2.0.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Autobooks",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "Autobooks",
@@ -18,20 +18,20 @@ let package = Package(
         // Main Autobooks SDK that requires triPOSMobileSDK
         .binaryTarget(
             name: "Autobooks",
-            url: "https://github.com/autobooks/autobooks-ios/raw/1.12.1/ZippedFrameworks/Autobooks.xcframework.zip",
-            checksum: "e6c17936604f9913b74a1a154208f3d3cec02d722dad833615c2211e8f69965e"
+            url: "https://github.com/autobooks/autobooks-ios/raw/2.0.0/ZippedFrameworks/Autobooks.xcframework.zip",
+            checksum: "1a07c7176bfc94b93b47643a8083848a6dc0792a07e09872833fd8fbca74ce75"
         ),
         // Fiserv-only version that does NOT require triPOSMobileSDK
         .binaryTarget(
             name: "Autobooks-FiservOnly",
-            url: "https://github.com/autobooks/autobooks-ios/raw/1.12.1/ZippedFrameworks/Autobooks-FiservOnly.xcframework.zip",
-            checksum: "3e0f6c6ac595aeb27caeb0db558f58bf788125a348fdc97853854d98344e276c"
+            url: "https://github.com/autobooks/autobooks-ios/raw/2.0.0/ZippedFrameworks/Autobooks-FiservOnly.xcframework.zip",
+            checksum: "364bf0758a94c3c1b2e52f75d209dd073bfe58beaf8771e4ecb0bd90699f1bc2"
         ),
         // triPOS dependency for Autobooks
         .binaryTarget(
             name: "triPOSMobileSDK",
-            url: "https://github.com/autobooks/autobooks-ios/raw/1.12.1/ZippedFrameworks/triPOSMobileSDK.xcframework.zip",
-            checksum: "b221e6b05bf75f5fa928149c5b691316ab4c1cdd55d158d747eec7a0095b72b0"
+            url: "https://github.com/autobooks/autobooks-ios/raw/2.0.0/ZippedFrameworks/triPOSMobileSDK.xcframework.zip",
+            checksum: "5d6236920f610cd3aadd2c8a45e65f5c278d05769936ba12fcc5a59929c8fac2"
         )
     ]
 )
